50862e5b2d62d941651a01eecdffd0d5d6884858
Simar Kochar <simark8640@gmail.com>
2025-04-05 21:41:21 +0530

    commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,110 @@
+import { Link } from "react-router-dom";
+import { HiMiniBars3CenterLeft, HiOutlineHeart, HiOutlineShoppingBag, HiOutlineShoppingCart } from "react-icons/hi2";
+import { IoSearchOutline } from "react-icons/io5";
+import { HiOutlineUser } from "react-icons/hi";
+
+import avatarImg from "../assets/avatar.png"
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
+
+const navigation = [
+    {name: "Dashboard", href:"/user-dashboard"},
+    {name: "Orders", href:"/orders"},
+    {name: "Cart Page", href:"/cart"},
+    {name: "Check Out", href:"/checkout"},
+]
+
+const Navbar = () => {
+
+
+
+    const  [isDropdownOpen, setIsDropdownOpen] = useState(false)
+    const cartItems = useSelector(state => state.cart.cartItems);
+   
+    const CurrentUser = true;
+
+
+    const token = localStorage.getItem('token');
+  
+    return (
+      <header className="max-w-screen-2xl mx-auto px-4 py-6">
+        <nav className="flex justify-between items-center">
+          {/* left side */}
+          <div className="flex items-center md:gap-16 gap-4">
+            <Link to="/">
+              <HiMiniBars3CenterLeft className="size-6" />
+            </Link>
+
+            {/* search input */}
+            <div className="relative sm:w-72 w-40 space-x-2">
+              <IoSearchOutline className="absolute inline-block left-3 inset-y-2" />
+
+              <input
+                type="text"
+                placeholder="Search here"
+                className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
+              />
+            </div>
+          </div>
+
+          {/* rigth side */}
+          <div className="flex items-center gap-4 relative md:space-x-3">
+            <div>
+              {CurrentUser ? (
+                <>
+                <button onClick={() => setIsDropdownOpen(!isDropdownOpen)} className="relative">
+                    <img src={avatarImg} alt="" className={`size-7 rounded-full ${
+                        CurrentUser ? "border-2 border-primary" : ""
+                    }`} />
+                </button>
+                {/* show dropdown  */}
+                {
+                    isDropdownOpen && (
+                        <div className="absolute top-10 right-0 bg-white shadow-md p-4 rounded
+                        w-48 z-10">
+                            <ul>
+                                {
+                                    navigation.map((item) => (
+                                        <li key={item.name} onClick={() =>{
+                                            setIsDropdownOpen(false);
+                                        }} className="py-2">
+                                            <Link to={item.href} className="block text-gray-700 hover:text-primary">
+                                                {item.name}
+                                            </Link>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    )
+                }
+                </>
+              ) : (
+                <Link to="/login">
+                  <HiOutlineUser className="size-6" />
+                </Link>
+              )}
+            </div>
+            <HiOutlineUser className="size-6" />
+            <button className="hidden sm:block relative">
+              <HiOutlineHeart className="size-6" />
+            </button>
+
+            <Link
+              to="/cart"
+              className="bg-primary p-1 sm:px-6 px-2 flex items-center rounded-md"
+            >
+              <HiOutlineShoppingBag>
+                <span className="absolute top-0 right-0 bg-red-600 text-white py-1 px-2 rounded-full text-xs font-bold">
+                  0
+                </span>
+              </HiOutlineShoppingBag>
+            </Link>
+          </div>
+        </nav>
+      </header>
+    );
+}
+
+export default Navbar;
---
+++ frontend/src/pages/home/TopSellers.jsx
@@ -0,0 +1,81 @@
+import React , {useEffect, useState}  from 'react'
+import BookCard from '../books/BookCard';
+
+import{ Swiper, SwiperSlide } from 'swiper/react';
+import{Navigation, Pagination} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+const categories = ["Choose a genre", "Business", "Fiction", "Horror", "Adventure"];
+
+const TopSellers = () => {
+  
+    const[books, setBooks] = useState([])
+    const[selectesdCategory, setSelectedCategory] = useState("Choose a genre")
+
+    useEffect(() => {
+        fetch("books.json").then((res) => res.json())
+        .then((data) => setBooks(data))
+     }, [])
+
+     const filterBooks = selectesdCategory === "Choose a genre" ? books : books.filter((book) => book.category === selectesdCategory.toLowerCase())
+
+  return (
+    <div className="py-10 ">
+      <h1 className="text-3xl font-semibold mb-6">Top Sellers</h1>
+      {/* category filtering */}
+      <div>
+        <select
+          onChange={(e) => setSelectedCategory(e.target.value)}
+          name="category"
+          id="category"
+          className="border bg-[#EAEAEA] border-gray-300 rounded-md p-2 focus:outline-none"
+        >
+          {categories.map((category, index) => (
+            <option value={category} key={index}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        navigation={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          }
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {
+        filterBooks.length > 0 &&
+        filterBooks.map((book, index) => (
+          <SwiperSlide>
+            <BookCard key={index} book={book} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+
+export default TopSellers
